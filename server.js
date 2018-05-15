bb109ae0f09de2a40cd163f76dd153518128bee2
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-05-15 16:00:35 -0500

    making porting better for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
